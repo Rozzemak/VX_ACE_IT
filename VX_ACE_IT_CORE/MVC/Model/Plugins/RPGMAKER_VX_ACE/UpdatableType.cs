8d8cf8db1ba95c98af883763a14aa74a00d23d44
Marek Slavka <rozzemak@gmail.com>
2018-10-13 04:11:56 +0200

    Updatable type impl, updatableType config basic imple idea.

---
+++ VX_ACE_IT_CORE/MVC/Model/Plugins/RPGMAKER_VX_ACE/UpdatableType.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using VX_ACE_IT_CORE.Debug;
+using VX_ACE_IT_CORE.MVC.Model.Async;
+using VX_ACE_IT_CORE.MVC.Model.GameProcess;
+using VX_ACE_IT_CORE.MVC.Model.Plugins.RPGMAKER_VX_ACE.VX_ACE_TYPES;
+
+namespace VX_ACE_IT_CORE.MVC.Model.Plugins.RPGMAKER_VX_ACE
+{
+    public class UpdatableType<T> : BaseAsync<object>
+    {
+        public readonly T Type;
+        public Dictionary<FieldInfo, List<List<IntPtr>>> Offsets = new Dictionary<FieldInfo, List<List<IntPtr>>>();
+
+        public UpdatableType(BaseDebug debug, ProcessMethods processMethods, T type, Dictionary<string, List<List<IntPtr>>> offsets)
+            : base(debug, processMethods._gameProcess)
+        {
+            this.Type = type;
+            Init(offsets);
+        }
+
+        void Init(Dictionary<string, List<List<IntPtr>>> offsets)
+        {
+            foreach (FieldInfo fieldInfo in Type.GetType().GetFields())
+            {
+                // Create plugin config, where will be desearialised class.
+                // idea -> could create entire from serializable document ? Let´s say player could be defined in txt.
+                // But that would require javascript dynamic access to object.... welp :-D
+                string fiName = offsets.Keys.FirstOrDefault((key => key == fieldInfo.Name));
+                if (fieldInfo.Name == fiName)
+                {
+                    offsets.TryGetValue(fieldInfo.Name, out var intPtrs);
+                    if (intPtrs != null)
+                        Offsets.Add(fieldInfo, intPtrs);
+                }
+                // Something like this. Just create usable config file for this.
+            }
+
+            if (this.Type.GetType().GetFields().Length < Offsets.Count)
+            {
+                // Create OnOffsetUpdate delegate.
+                Debug.AddMessage<object>(new Message<object>(
+                    "[" + GetType().Name + "][" + this.Type.GetType().Name + "] more offsets loaded than there are fields in the class. Check your config for additional lines.",
+                    MessageTypeEnum.Indifferent));
+            }
+            else
+            if (this.Type.GetType().GetFields().Length == Offsets.Count)
+            {
+                // Create OnOffsetUpdate delegate.
+                Debug.AddMessage<object>(new Message<object>(
+                    "[" + GetType().Name + "][" + this.Type.GetType().Name + "] all offsets loaded succesfully.",
+                    MessageTypeEnum.Standard));
+            }
+            else if (this.Type.GetType().GetFields().Length > 0)
+            {
+                Debug.AddMessage<object>(new Message<object>(
+                    "[" + GetType().Name + "][" + this.Type.GetType().Name + "] only some of the offsets were loaded.",
+                    MessageTypeEnum.Indifferent));
+                //Offsets.TryGetValue(typeof(Player).GetField("Hp"), out var list);
+                var dict = string.Join(";", Offsets);
+                Debug.AddMessage<object>(new Message<object>(
+                    ToDebugString(Offsets)
+                    ,MessageTypeEnum.Standard));
+            }
+            else if (this.Type.GetType().GetFields().Length == 0)
+            {
+                Debug.AddMessage<object>(new Message<object>(
+                    "[" + GetType().Name + "][" + this.Type.GetType().Name + "] none offsets were loaded.",
+                    MessageTypeEnum.Error));
+            }
+            else { } // Wtf hapened here ?!
+        }
+
+
+        public string ToDebugString(IDictionary<FieldInfo, List<List<IntPtr>>> dictionary)
+        {
+            return "{" + string.Join(",", dictionary.Select(kv => kv.Key.Name + "=" + WriteInpPtrList(kv.Value)).ToArray()) + "}";
+        }
+
+        private string WriteInpPtrList(List<List<IntPtr>> lists)
+        {
+            //I know this is not nice solution, but what gives. No performance benefit 
+            //from optimalisation, since this is just a init.
+            string strList = "";
+            foreach (var list in lists)
+            {
+                strList += "{";
+                foreach (IntPtr val in list)
+                {
+                    strList += "["+ val.ToString("X") +"]";
+                }
+                if (list.Count == 0) strList += "empty";
+                strList += "}";
+            }
+            return strList;
+        }
+    }
+} 
--- VX_ACE_IT_CORE/MVC/Model/Plugins/RPGMAKER_VX_ACE/VxAceModule.cs
+++ VX_ACE_IT_CORE/MVC/Model/Plugins/RPGMAKER_VX_ACE/VxAceModule.cs
@@ -18,27 +18,50 @@ namespace VX_ACE_IT_CORE.MVC.Model.Plugins.RPGMAKER_VX_ACE
 
         public IntPtr RgssBase;
 
+        public UpdatableType<Player> PlayerUpdatable;
+
         public VxAceModule(BaseDebug baseDebug, ProcessMethods processMethods, int precision = 17)
         : base(baseDebug, processMethods._gameProcess, precision)
         {
             _processMethods = processMethods;
             UpdateBaseAddress();
+
+            InitUpdatables(baseDebug, processMethods, precision);
+        }
+
+        public void InitUpdatables(BaseDebug debug, ProcessMethods processMethods, int precision)
+        {
+            PlayerUpdatable = new UpdatableType<Player>(debug, processMethods,
+                new Player(), new Dictionary<string, List<List<IntPtr>>>()
+                {
+                    {"Hp", new List<List<IntPtr>>()
+                    {
+                        new List<IntPtr>(){ new IntPtr(0x25A8B0), new IntPtr(0x30), new IntPtr(0x18), new IntPtr(0x20), new IntPtr((0x38))},
+                        new List<IntPtr>(){}, // <- other possible offset value if multipointer rpm fails.
+                    }}, // <- Do this for each field. Also, if we happen to have more fields than editable values.. 
+                    // 2 solutions, 1a) do not do it -> Just create another type like DrawablePlayer or something.
+                    // 1b) Create readable field name rule like 'FieldName_engineStat' 
+                    {"Mana", new List<List<IntPtr>>()
+                    {
+                        new List<IntPtr>(){ new IntPtr(0x25A8B0), new IntPtr(0x30), new IntPtr(0x18), new IntPtr(0x20), new IntPtr((0x38+0x4))},
+                    }},
+                });
         }
 
         public void UpdateBaseAddress()
         {
-            if(!(_processMethods._gameProcess.GetModuleAddresByName(ModuleName) is null))
-            new Task(() =>
-            {
-                while (true)
+            if (!(_processMethods._gameProcess.GetModuleAddresByName(ModuleName) is null))
+                new Task(() =>
                 {
-                    // if there is any problem, exchange will return previous value. useful ?
-                    Interlocked.Exchange(ref RgssBase, _processMethods._gameProcess.GetModuleAddresByName(ModuleName).BaseAddress);
-                    // NOOOOO RgssBase = _processMethods._gameProcess.GetModuleAddresByName(ModuleName).BaseAddress; 
-                    // There could be problem, with not enough updates for base adress. time/2 should work then.
-                    Thread.Sleep(Precision);
-                }
-            }).Start();
+                    while (true)
+                    {
+                        // if there is any problem, exchange will return previous value. useful ?
+                        Interlocked.Exchange(ref RgssBase, _processMethods._gameProcess.GetModuleAddresByName(ModuleName).BaseAddress);
+                        // NOOOOO RgssBase = _processMethods._gameProcess.GetModuleAddresByName(ModuleName).BaseAddress; 
+                        // There could be problem, with not enough updates for base adress. time/2 should work then.
+                        Thread.Sleep(Precision);
+                    }
+                }).Start();
         }
 
         // This is useless, can´t think of usefull case.
